f8272c268ed5a9bab2947f6e400605889a311186
Maxx Sanner <maxxsanner105@gmail.com>
2020-08-07 16:09:06 -0700

    added some basic functionality for adding title and description

---
+++ READMETEST.md
@@ -0,0 +1,3 @@
+Title: 12345
+Description: 0987654
+Table of Contents: dhgfnfds
--- index.js
+++ index.js
@@ -1,12 +1,42 @@
-const questions = [
+const fs = require("fs");
+const inquirer = require("inquirer");
 
+const questions = [
+    {
+        type: "input",
+        message: "What is your Project Title?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "What is your Description?",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "What is your Table of Contents?",
+        name: "contents"
+    }
 ];
 
-function writeToFile(fileName, data) {
+const writeToFile = (fileName, data) => {
+    fs.writeFile(fileName, data, function(err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("SUCCESS");
+    })
 }
 
-function init() {
-
+const init = () => {
+    inquirer.prompt(questions).then(function(response){
+        const { title } = response;
+        const { description } = response;
+        const { contents } = response;
+        writeToFile("READMETEST.md", `Title: ${title}` + '\n' +  `Description: ${description}` + '\n' + `Table of Contents: ${contents}`);
+    });
 }
 
 init();
+
+
